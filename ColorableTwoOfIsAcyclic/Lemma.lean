1b789055c4c6914e00448a06e38608f33b5bd1b6
hmiguel <miguel.hidalgo@ynov.com>
2025-07-24 16:22:56 +0200

    sTree.coloring_two_of_elem

--- ColorableTwoOfIsAcyclic/Lemma.lean
+++ ColorableTwoOfIsAcyclic/Lemma.lean
@@ -11,7 +11,15 @@ lemma IsTree.diff_dist_adj (hG : G.IsTree) (u v w : V) (hadj : SimpleGraph.Adj G
 
 noncomputable def IsTree.coloring_two_of_elem (hG : G.IsTree) (u : V) : G.Coloring (Fin 2) :=
   Coloring.mk (fun v ↦ ⟨G.dist u v % 2, Nat.mod_lt (G.dist u v) Nat.zero_lt_two⟩) <| by
-    sorry
+  intro v w hadj h
+  simp only [Fin.mk.injEq] at h
+  have := hG.diff_dist_adj u v w hadj
+  obtain hA | hB := hG.diff_dist_adj u v w hadj
+  · rw [hA] at h
+    omega
+  · rw [← hB] at h
+    omega
+
 
 noncomputable def IsTree.coloring_two (hG : G.IsTree) : G.Coloring (Fin 2) :=
   let u : V := Classical.choice inst
