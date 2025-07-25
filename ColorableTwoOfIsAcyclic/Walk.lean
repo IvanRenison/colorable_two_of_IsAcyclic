8da77f7d79b47794d00e00109e1fd53826277b5a
hmiguel <miguel.hidalgo@ynov.com>
2025-07-25 10:42:31 +0200

    Walk.eq_iff_support_eq

--- ColorableTwoOfIsAcyclic/Walk.lean
+++ ColorableTwoOfIsAcyclic/Walk.lean
@@ -9,6 +9,10 @@ lemma Walk.eq_iff_support_eq {u v : V} (p q : G.Walk u v) :
   sorry
 
 lemma Walk.mem_concat_support {u v w : V} (p : G.Walk u v) (hadj : G.Adj v w) :
-    v ∈ (p.concat hadj).support := sorry
+    v ∈ (p.concat hadj).support := by
+  rw [Walk.concat_eq_append]
+  rw [Walk.mem_support_append_iff]
+  left
+  exact Walk.end_mem_support p
 
 end SimpleGraph
