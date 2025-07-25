e19790e3a85a6f8b4554fa616d22f1d95f9e7a09
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2025-07-25 09:47:39 +0200

    Walk.Lean

--- ColorableTwoOfIsAcyclic.lean
+++ ColorableTwoOfIsAcyclic.lean
@@ -1,2 +1,3 @@
 import ColorableTwoOfIsAcyclic.Lemma
 import ColorableTwoOfIsAcyclic.List
+import ColorableTwoOfIsAcyclic.Walk
---
+++ ColorableTwoOfIsAcyclic/Walk.lean
@@ -0,0 +1,14 @@
+import Mathlib
+
+namespace SimpleGraph
+
+variable {V : Type} {G : SimpleGraph V}
+
+lemma Walk.eq_iff_support_eq {u v : V} (p q : G.Walk u v) :
+    p = q ↔ p.support = q.support := by
+  sorry
+
+lemma Walk.mem_concat_support {u v w : V} (p : G.Walk u v) (hadj : G.Adj v w) :
+    v ∈ (p.concat hadj).support := sorry
+
+end SimpleGraph
