635e05a34c874fad46bead5d56186c1f40dd09d0
hmiguel <miguel.hidalgo@ynov.com>
2025-07-25 15:48:12 +0200

    Walk.eq_iff_support_eq

--- ColorableTwoOfIsAcyclic/Walk.lean
+++ ColorableTwoOfIsAcyclic/Walk.lean
@@ -4,6 +4,34 @@ namespace SimpleGraph
 
 variable {V : Type} {G : SimpleGraph V}
 
+lemma Walk.eq_iff_support_eq {u v : V} (p q : G.Walk u v) :
+    p = q ↔ p.support = q.support := by
+  constructor
+  · intro h
+    rw [h]
+  · intro h
+    induction q with
+    | nil =>
+      simp at h
+      rw [← nil_iff_eq_nil, nil_iff_support_eq]
+      exact h
+    | cons x u ih =>
+      rename_i h' a b c
+      induction p with
+      | nil =>
+        simp at h
+      | cons y w ih' =>
+        rename_i a' b' c'
+        simp only [support_cons, List.cons.injEq, true_and] at h
+        have : b' = b := by
+          rw [← head_support w]
+          simp_rw [h]
+          simp
+        subst this
+        have := ih _ h
+        subst this
+        rfl
+
 lemma Walk.mem_support_concat_of_mem_support {u v w : V} (p : G.Walk u v) (hadj : G.Adj v w)
     {x : V} (hx : x ∈ p.support) : x ∈ (p.concat hadj).support := by
   rw [concat_eq_append, mem_support_append_iff]
